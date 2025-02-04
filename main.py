d4624e97de693c42e161a388fc78daf3743dc2aa
Sofia Canossa <sofiacanossa5@gmail.com>
2025-02-04 11:12:42 +0000

    my first additions

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from flet import *
+import flet as ft
+from flet import Page, Container, Text, AppBar
 
 def main(page:Page):
-	page.appbar = AppBar(title=Text("Yooutube app"))
+	page.appbar = AppBar(title=Text("New app!!!"))
 	page.add(
 		Container(
 			bgcolor="red",
 			padding=10,
-			alignment=alignment.center,
-			content=Text("hello",size=30,weight="bold")
-			)
-		)
-app(main)
+			alignment=ft.alignment.center,
+			content=Text("hello",size=30,weight="bold"),
+			),
+		Container(
+			bgcolor="blue",
+			padding=10,
+			alignment=ft.alignment.center,
+			content=Text("mum!",size=50,weight="bold"),
+			),
+		),
+	
+
+if __name__ == "__main__":
+	ft.app(main)
 
